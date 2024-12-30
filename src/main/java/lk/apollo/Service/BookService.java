04a4248a54fecad5549821b85cd4f94ea3cc99a8
shanemalka9 <shanemalka@outlook.com>
2024-12-30 09:19:49 +0530

    added dependacy injection to service

--- src/main/java/lk/apollo/Service/BookService.java
+++ src/main/java/lk/apollo/Service/BookService.java
@@ -1,6 +1,17 @@
 package lk.apollo.Service;
 
+import lk.apollo.model.Book;
+import lk.apollo.repository.BookRepository;
+
 public class BookService {
 
+    BookRepository bookRepository;
 
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void addBook(Book book) {
+        bookRepository.save(book);
+    }
 }
